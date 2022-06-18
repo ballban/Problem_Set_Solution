db05ff7655b25b200a8991b223ba7d459f17d740
ballban <ballban@hotmail.com>
2022-06-19 01:11:57 +0900

    >.>

---
+++ 198_House_Robber.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return nums[0]
+        dp = [0, 0] + nums
+        for i in range(3, len(nums) + 2):
+            dp[i] += max(dp[i - 2], dp[i - 3])
+        return max(dp[-1], dp[-2])
